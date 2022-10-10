50d6262c180509da5ac65da116b24ef7caf08879
lavish <lavishjain36@gmil.com>
2022-10-10 13:05:12 +0530

    new changes

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className='text-center py-3'>Copyright  &copy; ProShop App</Col>
+          <Col className='text-center py-3'>Copyright  &copy; ProShop New App</Col>
         </Row>
       </Container>
     </footer>
